3a4d97d066d81e5184fd77f8d13b2f4bdd0c4833
Sanskar-Rijal <rijalsanskar@gmail.com>
2026-05-02 21:44:44 +0545

    made /health to check server status

---
+++ Social/cmd/api/api.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config 
+}
+
+type config struct {
+	addr string //address of the port we want to listen on
+}
+
+
+func (app *application) mount() http.Handler {
+	// mux := http.NewServeMux()
+
+	// mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	router := chi.NewRouter()
+
+
+// A good base middleware stack
+    router.Use(middleware.RequestID)
+    router.Use(middleware.RealIP)
+    router.Use(middleware.Logger)
+    router.Use(middleware.Recoverer)
+
+	router.Route("/v1", func (r chi.Router){
+		r.Get("/health",app.healthCheckHandler)
+	})
+
+
+	return router
+}
+
+func (app *application) run(mux http.Handler) error {
+
+	server := &http.Server{
+		Addr: app.config.addr,
+		Handler: mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout: time.Second *10,
+		IdleTimeout: time.Minute,
+	}
+	log.Printf("Server has started at %s",app.config.addr)
+	return server.ListenAndServe()
+}
