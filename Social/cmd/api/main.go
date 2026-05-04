8dec3fa6b2f93521f3e24125bda56a63c12da747
Sanskar-Rijal <rijalsanskar@gmail.com>
2026-05-04 07:38:04 +0545

    creating repository and added hot reloading

--- Social/cmd/api/main.go
+++ Social/cmd/api/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
+	"social/internal/env"
+	"social/internal/store"
+)
 
 func main() {
+
+	store := store.NewPostgresStorage(nil)
+
 	app := &application{
 		config: config{
-			addr: ":8080",
+			addr: env.GetString("ADDR",":8080"),
 		},
+		store: store,
 	}
+
 
 	mux := app.mount()
 	log.Fatal(app.run(mux))
